139628a23bac9a55f5caa9c2030e0d63336e9511
David Duran <dadch1404@gmail.com>
2024-03-10 17:59:21 -0500

    fix: URL encoding in getFeedListing function

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 const corsUrl = "https://rss-to-json.cae.workers.dev/?url=";
 
-export const getFeedListing = url => axios.get(`${corsUrl}${url}`);
+export const getFeedListing = url => axios.get(`${corsUrl}${encodeURIComponent(url)}`);
